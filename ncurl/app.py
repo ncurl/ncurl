01a9de420e2dfb66e0342b446a1d51e78702b594
bohan <bohanzhang@foxmail.com>
2020-04-27 12:55:41 +0800

    upgrade

--- ncurl/app.py
+++ ncurl/app.py
@@ -6,19 +6,25 @@ import sys
 from pygments import highlight
 from pygments.lexers.javascript import JavascriptLexer
 from pygments.formatters.terminal import TerminalFormatter
+from pygments.lexers.shell import BashLexer
 
-from ncurl.curl_utils import curl_command_to_response
+from ncurl.utils import curl_command_to_response, response_to_head_str
 
 
 def do_curl():
     command = ['curl'] + sys.argv[1:]
 
     response = curl_command_to_response(command)
-    result = response.json()
-    # headers_result = highlight(json.dumps(response.headers, indent=4), BashLexer(), TerminalFormatter())
-    result = highlight(json.dumps(result, indent=4), JavascriptLexer(), TerminalFormatter())
-    # print(headers_result)
-    print(result)
+    head_str = response_to_head_str(response)
+    headers_result = highlight(head_str, BashLexer(), TerminalFormatter())
+    print(headers_result)
+
+    try:
+        result = highlight(json.dumps(response.json(), indent=4, sort_keys=True), JavascriptLexer(), TerminalFormatter())
+        print(result)
+    except ValueError:
+        result = highlight(response.content.decode("utf-8"), JavascriptLexer(), TerminalFormatter())
+        print(result)
 
 
 def main():
---
+++ ncurl/utils.py
@@ -0,0 +1,48 @@
+import json
+import shlex
+
+import uncurl
+from pygments import highlight
+from pygments.formatters.terminal import TerminalFormatter
+from pygments.lexers.javascript import JavascriptLexer
+from pygments.lexers.text import TexLexer
+from pygments.lexers.shell import BashLexer
+from requests import Response, request
+
+
+def curl_command_to_response(command) -> Response:
+    """
+    change curl to request
+    :param command:
+    :return: Request
+    """
+    context = uncurl.parse_context(shlex.join(command))
+    data = context.data.encode('utf-8') if context.data else None
+    return request(method=context.method, url=context.url, headers=context.headers, data=data)
+
+
+def response_to_head_str(response: Response) -> str:
+    """
+    response status and header to string
+    :param response:
+    :return:
+    """
+    head_str = ''
+    head_str += f'HTTP {response.status_code} {response.reason}\n'
+    for key, value in response.headers.items():
+        head_str += f"{key}: {value}\n"
+    return head_str
+
+
+def print_response(response: Response) -> None:
+    head_str = response_to_head_str(response)
+    headers_result = highlight(head_str, BashLexer(), TerminalFormatter())
+    print(headers_result)
+
+    try:
+        result = highlight(json.dumps(response.json(), indent=4, sort_keys=True), JavascriptLexer(),
+                           TerminalFormatter())
+        print(result)
+    except ValueError:
+        result = highlight(response.content.decode("utf-8"), TexLexer(), TerminalFormatter())
+        print(result)
